7193dace42804b5dd1c76d76626c27b0270c2d90
Sid Mane <mane.si@northeastern.edu>
2025-04-13 22:03:48 -0400

    changed dqn script

--- scripts/dqn.sh
+++ scripts/dqn.sh
@@ -7,14 +7,16 @@ RESULTS_DIR="../results"
 EXE="python3 ./entrypoints/train_dqn.py"
 
 printf "Title: "; read title
-OUTPUT_DIR="$RESULTS_DIR/$title-$DATE"
+DIR_NAME="$title-$DATE"
+OUTPUT_DIR="$RESULTS_DIR/$DIR_NAME"
 
 mkdir -p "$OUTPUT_DIR"
 # zip the results and delete folder
 function exit_handler() {
     echo -e "\n\nZipping $OUTPUT_DIR..."
-    zip "$OUTPUT_DIR.zip" "$OUTPUT_DIR" 2> /dev/null 1>&2
-    rm -r "$OUTPUT_DIR"
+    cd $RESULTS_DIR
+    zip "$DIR_NAME.zip" "$DIR_NAME" 2> /dev/null 1>&2
+    rm -r "$DIR_NAME"
 }
 trap exit_handler EXIT
 
